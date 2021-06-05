efc4bd3647075cbbeb2ae66875273164df447041
Anthony Woodward <P17213923@my365.dmu.ac.uk>
2021-06-05 20:14:46 +0100

    Fixed Bugs

---
+++ Submerse/Assets/playonvid.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class playonvid : MonoBehaviour
+{
+
+    public Animator anim;
+    public VideoPlayer vp;
+    public SkinnedMeshRenderer skin;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (vp.isPrepared)
+        {
+            anim.SetBool("isPrepared", true);
+            StartCoroutine("appear");
+        }
+    }
+
+    IEnumerator appear()
+    {
+        yield return new WaitForSeconds(0.2f);
+        skin.enabled = true;
+    }
+}
